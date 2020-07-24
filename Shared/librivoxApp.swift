496abe8cd83a53129f01f7984c28d7aaabfecb1a
Weizhi Xie <deexie1990@gmail.com>
2020-07-24 10:43:37 +0800

    Initial Commit

---
+++ Shared/librivoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  librivoxApp.swift
+//  Shared
+//
+//  Created by weizhi on 2020/7/24.
+//
+
+import SwiftUI
+
+@main
+struct librivoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
